9af1c5f9302d7d273748c7dfbc381ede501bee00
Amynem <amy.lauren98@hotmail.com>
2024-10-02 22:18:57 +0200

    ejercicio 3

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -40,8 +40,18 @@ findLongestWord()
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    if ( numbers.length === 1){
+        return numbers[0];
+    }
+    let sum = 0;
+    for( let i = 0; i < numbers.length; i++) { 
+        sum += numbers[i];
+    }
+    return sum;
+}
 
+sumNumbers()
 
 
 
